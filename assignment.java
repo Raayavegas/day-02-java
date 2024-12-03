27ccde912b6763fbbdb6f247cb00f5e7500af8ee
Raayavegas <veeralarithika@gmail.com>
2024-12-03 10:57:22 +0530

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Ass
+{
+    public static void main(String args[])
+    {
+        int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+
+    }
+}
