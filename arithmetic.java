b68cb5887ad7d7122231fd4ea079b9ab7bbda7e4
Raayavegas <veeralarithika@gmail.com>
2024-12-03 10:14:45 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Arith
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a+=b;
+        a-=b;
+        a*=b;
+        a/=b;
+        a%=b;
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+        
+
+    }
+}
