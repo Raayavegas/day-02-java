1e52fb256e5b0b8bef98350272bf3108eb3c35bb
Raayavegas <veeralarithika@gmail.com>
2024-12-03 10:22:13 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class com
+{
+    public static void main(String args[])
+    {
+        int a=24;
+        int b=42;
+        System.out.println(a==b);
+        System.out.println(a>=b);
+        System.out.println(a!=b);
+        System.out.println(a<=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+    }
+}
