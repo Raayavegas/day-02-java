04c956f69ac63d4582a43ec472e2e62c2a0838ab
Raayavegas <veeralarithika@gmail.com>
2024-12-04 10:15:46 +0530

    Add files via upload

---
+++ xor.java
@@ -0,0 +1,15 @@
+class A 
+{
+    public static void main(String args[])
+    {
+        int a=10,b=20;
+        if((a+b)==(a^b))
+        {
+            System.out.println("equal");
+        }
+        else
+        {
+            System.out.println("not equal");
+        }
+    }
+}
